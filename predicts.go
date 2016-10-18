6d6904a113349353e53eafa9154b153446e8aabd
MohamedBassem <medoox240@gmail.com>
2016-10-18 03:34:11 +0200

    Adding the predicts implementation

---
+++ predicts.go
@@ -0,0 +1,23 @@
+package main
+
+type Predict interface {
+	String() string
+}
+
+type PredictImpl struct {
+	Operator string
+}
+
+func (p PredictImpl) String() string {
+	return p.Operator
+}
+
+var (
+	EqualPredict              = PredictImpl{Operator: "="}
+	NotEqualPredict           = PredictImpl{Operator: "<>"}
+	GreaterThanPredict        = PredictImpl{Operator: ">"}
+	GreaterThanOrEqualPredict = PredictImpl{Operator: ">="}
+	SmallerThanPredict        = PredictImpl{Operator: "<"}
+	SmallerThanOrEqualPredict = PredictImpl{Operator: "<="}
+	LikePredict               = PredictImpl{Operator: "LIKE"}
+)
