95ae313b0c906edf537b3afc802e7b2f26533567
MohamedBassem <medoox240@gmail.com>
2016-10-19 19:48:22 +0200

    Adding tests for save and update

---
+++ gormgen_basic_test.go
@@ -0,0 +1,59 @@
+package gormgen
+
+import (
+	"math/rand"
+	"testing"
+
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/stretchr/testify/suite"
+)
+
+type BasicTestSuite struct {
+	MainTestSuite
+}
+
+func TestBasicSuite(t *testing.T) {
+	suite.Run(t, new(BasicTestSuite))
+}
+
+type BasicModel struct {
+	Name string
+	Age  int
+}
+
+func randomBasicModel() *BasicModel {
+	return &BasicModel{
+		Name: generateRandomString(10),
+		Age:  rand.Intn(50),
+	}
+}
+
+func (b *BasicTestSuite) TestSaveCreate() {
+	model := randomBasicModel()
+	err := model.Save(b.getDBConn())
+	b.Require().Nil(err, "Saving shouldn't return an error")
+
+	db := b.getDBConn()
+	fetchedModel := &BasicModel{}
+	notFound := db.Find(fetchedModel, randomBasicModel).RecordNotFound()
+	b.Require().False(notFound, "The model should have been created")
+	b.Require().Equal(model, fetchedModel, "The fetched model should have been correctly saved")
+}
+
+func (b *BasicTestSuite) TestSaveUpdate() {
+	// Create and save a model
+	model := randomBasicModel()
+	b.getDBConn().Create(model)
+
+	// Update and save the model
+	model.Age = 0
+	err := model.Save(b.getDBConn())
+	b.Require().Nil(err, "Saving shouldn't return an error")
+
+	// Fetch the model by its name and assert that its the same
+	db := b.getDBConn()
+	fetchedModel := []BasicModel{}
+	db.Find(&fetchedModel, map[string]interface{}{"Name": model.Name})
+	b.Require().Equal(1, len(fetchedModel), "The database shouldn't create a new model but rather update the old one")
+	b.Require().Equal(model, &fetchedModel[0], "The fetched model should have been correctly updated")
+}
---
+++ structs_test.go
@@ -0,0 +1,119 @@
+package gormgen
+
+import "github.com/jinzhu/gorm"
+
+func (t *BasicModel) Save(db *gorm.DB) error {
+	return db.Save(t).Error
+}
+
+func (t *BasicModel) Delete(db *gorm.DB) error {
+	return db.Delete(t).Error
+}
+
+type BasicModelQueryBuilder struct {
+	order []string
+	where []struct {
+		prefix string
+		value  interface{}
+	}
+	limit  int
+	offset int
+}
+
+func (qb *BasicModelQueryBuilder) buildQuery(db *gorm.DB) *gorm.DB {
+	ret := db
+	for _, where := range qb.where {
+		ret = ret.Where(where)
+	}
+	for _, order := range qb.order {
+		ret = ret.Order(order)
+	}
+	ret = ret.Limit(qb.limit).Offset(qb.offset)
+	return ret
+}
+
+func (qb *BasicModelQueryBuilder) Count(db *gorm.DB) (int, error) {
+	var c int
+	res := qb.buildQuery(db).Model(&BasicModel{}).Count(&c)
+	if res.RecordNotFound() {
+		c = 0
+	}
+	return c, res.Error
+}
+
+func (qb *BasicModelQueryBuilder) First(db *gorm.DB) (*BasicModel, error) {
+	ret := &BasicModel{}
+	res := qb.buildQuery(db).First(ret)
+	if res.RecordNotFound() {
+		ret = nil
+	}
+	return ret, res.Error
+}
+
+func (qb *BasicModelQueryBuilder) QueryOne(db *gorm.DB) (*BasicModel, error) {
+	qb.offset = 1
+	ret, err := qb.QueryAll(db)
+	if len(ret) > 0 {
+		return &ret[0], err
+	} else {
+		return nil, err
+	}
+}
+
+func (qb *BasicModelQueryBuilder) QueryAll(db *gorm.DB) ([]BasicModel, error) {
+	ret := []BasicModel{}
+	err := qb.buildQuery(db).Find(&ret).Error
+	return ret, err
+}
+
+func (qb *BasicModelQueryBuilder) Limit(limit int) *BasicModelQueryBuilder {
+	qb.limit = limit
+	return qb
+}
+
+func (qb *BasicModelQueryBuilder) Offset(offset int) *BasicModelQueryBuilder {
+	qb.offset = offset
+	return qb
+}
+
+func (qb *BasicModelQueryBuilder) WhereName(p Predict, value string) *BasicModelQueryBuilder {
+	qb.where = append(qb.where, struct {
+		prefix string
+		value  interface{}
+	}{
+		"name " + p.String(),
+		value,
+	})
+	return qb
+}
+
+func (qb *BasicModelQueryBuilder) OrderByName(asc bool) *BasicModelQueryBuilder {
+	order := "DESC"
+	if asc {
+		order = "ASC"
+	}
+
+	qb.order = append(qb.order, "name "+order)
+	return qb
+}
+
+func (qb *BasicModelQueryBuilder) WhereAge(p Predict, value int) *BasicModelQueryBuilder {
+	qb.where = append(qb.where, struct {
+		prefix string
+		value  interface{}
+	}{
+		"age " + p.String(),
+		value,
+	})
+	return qb
+}
+
+func (qb *BasicModelQueryBuilder) OrderByAge(asc bool) *BasicModelQueryBuilder {
+	order := "DESC"
+	if asc {
+		order = "ASC"
+	}
+
+	qb.order = append(qb.order, "age "+order)
+	return qb
+}
